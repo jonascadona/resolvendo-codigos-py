c67427d08a0791259c96abca938125ec3540c4e6
jonascadona <jonascadona891@gmail.com>
2024-12-17 15:10:21 +0000

    resolvendo os desafios do projeto - 2/3

---
+++ repeat_txt.py
@@ -0,0 +1,6 @@
+# Solicitar uma string e um número inteiro como entrada. 
+
+string = input("Digite uma string: ")
+numero = int(input("Digite um número inteiro: "))
+
+print((string + ' ') * numero)
